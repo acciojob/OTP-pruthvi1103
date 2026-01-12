82336e1c8c85c1a2f17cc569d64cd7c5a0468410
pruthvi1103 <pruthvirajrachakonda@gmail.com>
2026-01-12 12:10:54 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,18 @@
 //your JS code here. If required.
+const codes = document.querySelectorAll(".code");
+
+codes.forEach((code) => {
+  code.addEventListener("keyup", (e) => {
+    if (e.key === "Backspace") {
+      const prevSibling = code.previousElementSibling;
+      if (prevSibling) {
+        prevSibling.focus();
+      }
+      return;
+    }
+    const nextSibling = code.nextElementSibling;
+    if (nextSibling) {
+      nextSibling.focus();
+    }
+  });
+});
